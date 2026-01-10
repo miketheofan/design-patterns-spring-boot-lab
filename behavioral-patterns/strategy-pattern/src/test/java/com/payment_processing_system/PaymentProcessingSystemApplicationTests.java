b151a4e7ddafe610e4f535eaa35a352d7ff25793
miketheofan <miketheofan@gmail.com>
2026-01-10 13:42:20 +0200

    DPSB-B001: initialise Spring Boot project

---
+++ behavioral-patterns/strategy-pattern/src/test/java/com/payment_processing_system/PaymentProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment_processing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
